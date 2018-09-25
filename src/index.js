d7f2bcb3c779ff02731be919a2ece1250aff6d43
rudlevsky <rudlevsky@gmail.com>
2018-09-25 12:42:32 +0300

    all tests is ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let spltStr = equation.split(' ');
+  let first = +spltStr[0];
+  let second = +(spltStr[3] + spltStr[4]);
+  let third = +(spltStr[7] + spltStr[8]);
+
+  let discrim = Math.sqrt(Math.pow(second,2) - 4*first*third);
+  let mas = [];
+  if(discrim > 0){
+      mas.push(Math.round((-1)*second + discrim)/(2*first));
+      mas.push(Math.round(((-1)*second - discrim))/(2*first));
+  } else if (discrim == 0){
+      mas.push(Math.round(((-1)*second)/(2*first)));
+      mas.push(Math.round(((-1)*second)/(2*first)));
+  }
+  
+  return mas.sort((a,b) => a - b);
 }
+
